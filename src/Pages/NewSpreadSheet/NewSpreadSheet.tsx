78684cb8c473fae6fddaaf2d49361d985216c43b
Konrad Walas <kwalas314@gmail.com>
2022-09-13 06:06:50 +0200

    Set proper hours

--- src/Pages/NewSpreadSheet/NewSpreadSheet.tsx
+++ src/Pages/NewSpreadSheet/NewSpreadSheet.tsx
@@ -6,8 +6,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NewSpreadSheet = () => {
-  const [dateTimeFrom, setDateTimeFrom] = useState<Moment | null>(moment());
-  const [dateTimeTo, setDateTimeTo] = useState<Moment | null>(moment());
+  const [dateTimeFrom, setDateTimeFrom] = useState<Moment | null>(moment().hour(8).minute(0));
+  const [dateTimeTo, setDateTimeTo] = useState<Moment | null>(moment().hour(20).minute(0));
   const [eventName, setEventName] = useState<string>('Event');
 
   const handleChangeFrom = (newValue: Moment | null) => {
